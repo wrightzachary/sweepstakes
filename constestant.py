4b947b8b90e3448c06c210fcd0ae07670e86e9e2
wrightzachary <taylorzw96@gmail.com>
2021-06-22 11:09:38 -0500

    Added contestant class

---
+++ constestant.py
@@ -0,0 +1,7 @@
+class Contestant:
+    def __init__(self):
+        self.first_name = ""
+        self.last_name = ""
+        self.email = ""
+        self.address = ""
+        self.registration_number = 0
