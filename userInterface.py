98553fb5b555d3308eeadb6f3656ef26fe9c39d7
wrightzachary <taylorzw96@gmail.com>
2021-06-24 02:07:11 -0500

    On the user interface chaned all messages from print to UserInterface.display_message()

--- userInterface.py
+++ userInterface.py
@@ -15,7 +15,7 @@ class UserInterface:
 
     @staticmethod
     def display_contestant_info(contestants):
-        print(f'\t{contestants.values()}')
+        UserInterface.display_message(f'\t{contestants.values()}')
         UserInterface.display_sweepstakes_menu_options(contestants)
         # print(contestants.first_name)
         # print(contestants.last_name)
@@ -26,33 +26,33 @@ class UserInterface:
     @staticmethod
     def display_sweepstakes_info(sweepstakes):
         UserInterface.display_message(f'\tWelcome!')
-        print('')
-        print("\tPress -1- to exit to sweepstakes menu")
+        UserInterface.display_message('')
+        UserInterface.display_message("\tPress -1- to exit to sweepstakes menu")
 
     @staticmethod
     def display_sweepstakes_selections_menu(sweepstakes):
         sweepstakes_name = ""
-        print("\tWelcome to sweepstakes menu")
+        UserInterface.display_message("\tWelcome to sweepstakes menu")
         UserInterface.display_sweepstakes_menu_options(sweepstakes_name)
 
     @staticmethod
     def display_marketing_firm_menu_options(marketing_firm_name):
-        print("\tWelcome!")
-        print('')
-        print("\tPress -1- to create a sweepstake")
-        print("\tPress -2- to change firm name")
-        print("\tPress -3- to select a sweepstake")
-        print("\tPress -4- to register")
-        print('')
+        UserInterface.display_message("\tWelcome!")
+        UserInterface.display_message('')
+        UserInterface.display_message("\tPress -1- to create a sweepstake")
+        UserInterface.display_message("\tPress -2- to change firm name")
+        UserInterface.display_message("\tPress -3- to select a sweepstake")
+        UserInterface.display_message("\tPress -4- to register")
+        UserInterface.display_message('')
 
     @staticmethod
     def display_sweepstakes_menu_options(sweepstakes_name):
-        print('')
-        print("\tSweepstakes Menu")
-        print('')
-        print("\tPress -1- to register")
-        print("\tPress -2- to view contestants")
-        print("\tPress -3- to pick winner")
-        print("\tPress -4- to terminate session")
-        print('')
+        UserInterface.display_message('')
+        UserInterface.display_message("\tSweepstakes Menu")
+        UserInterface.display_message('')
+        UserInterface.display_message("\tPress -1- to register")
+        UserInterface.display_message("\tPress -2- to view contestants")
+        UserInterface.display_message("\tPress -3- to pick winner")
+        UserInterface.display_message("\tPress -4- to terminate session")
+        UserInterface.display_message('')
 
